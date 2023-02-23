7bbb2cff1ac1452b471224e6a0315b1d1d19adfd
Natalia Vignola <natalia.vignola@hotmail.com>
2023-02-23 17:25:56 -0300

    js repo 2

--- js/functions.js
+++ js/functions.js
@@ -1,63 +1,81 @@
-//VARIABLE//
-const nombreUsuario = prompt('Ingrese su nombre de usuario')
-const contrasena = prompt('Ingrese su contraseña')
-
-//FUNCION DE INGRESO//
 function IngresoDeUsuario() {
-    nombreUsuario
-    contrasena
+    var nombreUsuario = prompt('Ingrese su nombre de usuario')
+    var contrasena = prompt('Ingrese su contraseña')
     alert('¡Hola ' + nombreUsuario + '!')
 }
 
-//FUNCION DE SUSCRIPCIONES//
+
 function elegirSuscripcion() {
     alert('Presiona "Aceptar" para desplegar nuestros planes de suscripción mensual')
-    let tipoDeSuscripcion = prompt('Elige tu suscripción: \n 1-Everyday \n 2-Simple \n 3-Doble Shot')
-    switch (tipoDeSuscripcion){
-        case '1':
-            alert('¡Felicitaciones por su suscripción ' + nombreUsuario + '! \n El total de su compra es de $690')
+    let tipoDeSuscripcion = parseInt(prompt('Elige tu suscripción: \n 1-Everyday \n 2-Simple \n 3-Doble Shot'))
+    switch (tipoDeSuscripcion) {
+        case 1:
+            alert('¡Felicitaciones por su suscripción! \n del producto añadido es de $690')
             break
-        case '2':
-            alert('¡Felicitaciones por su suscripción ' + nombreUsuario + '! \n El total de su compra es de $900')
+        case 2:
+            alert('¡Felicitaciones por su suscripción! \n del producto añadido es de $900')
             break
-        case '3':
-            alert('¡Felicitaciones por su suscripción ' + nombreUsuario + '! \n El total de su compra es de $1700')
+        case 3:
+            alert('¡Felicitaciones por su suscripción! \n El del producto añadido es de $1700')
             break;
         default:
             alert('Ingrese opcion valida (1, 2 o 3)')
-            break
+            break;
     }
 }
 
-//FUNCION DE BOLSAS//
+
 function elegirBolsa() {
     alert('Presiona "Aceptar" para comprar nuestros cafes de especialidad')
-    let tipoDeBolsa = prompt('Elige tu café: \n 1-Brasil Crucera \n 2-Etiopía Najá \n 3-Guatemala GTA')
+    let tipoDeBolsa = parseInt(prompt('Elige tu café: \n 1-Brasil Crucera \n 2-Etiopía Najá \n 3-Guatemala GTA'))
     switch (tipoDeBolsa) {
-        case '1':
-            alert('¡Felicitaciones por su compra ' + nombreUsuario + '! \n El total de su compra es de $550')
-            break
-        case '2':
-            alert('¡Felicitaciones por su compra ' + nombreUsuario + '! \n El total de su compra es de $800')
-            break
-        case'3':
-            alert('¡Felicitaciones por su compra ' + nombreUsuario + '! \n El total de su compra es de $700')
+        case 1:
+            alert('¡Felicitaciones por su compra! \n El valor del producto añadido es de $550')
             break;
-            default:
-                alert('Ingrese opcion valida (1, 2 o 3)')
-                break
+        case 2:
+            alert('¡Felicitaciones por su compra! \n El del producto añadido es de $800')
+            break;
+        case 3:
+            alert('¡Felicitaciones por su compra! \n El del producto añadido es de $700')
+            break;
+        default:
+            alert('Ingrese opcion valida (1, 2 o 3)')
+            break;
     }
 }
 
-//FUNCION GENERAL DE INGRESO, SUSCRIPCION O COMPRA//
+
 function QueComprar() {
-    let suscripcionOCompra = prompt('¿Qué estás buscando? \n 1-Suscripción mensual \n 2-Compra')
-    if (suscripcionOCompra == '1') elegirSuscripcion();
-    if (suscripcionOCompra == '2') elegirBolsa();
+    let suscripcionOCompra = parseInt(prompt('¿Qué estás buscando? \n 1-Suscripción mensual \n 2-Compra'))
+    do {
+        if (suscripcionOCompra == 1) {
+            elegirSuscripcion();
+            break;
+        } else if (suscripcionOCompra == 2) {
+            elegirBolsa();
+            break;
+        } else {
+            alert('Ingrese opcion valida (1 o 2)');
+            suscripcionOCompra = parseInt(prompt('¿Qué estás buscando? \n 1-Suscripción mensual \n 2-Compra'));
+        }
+    } while (suscripcionOCompra === 1 || suscripcionOCompra === 2)
 }
 
-//PARA VISUALIZAR HTML MIENTRAS SE EJECUTAN LAS FUNCIONES//
-window.addEventListener('load',function(){
+
+function SeguirCompra() {
+    let quieroSeguirComprando = prompt('¿Queres seguir comprando? \n 1-Si \n 2-No');
+    if (quieroSeguirComprando === '1') {
+        QueComprar();
+        SeguirCompra();
+    } else {
+        alert('Nos vemos pronto por más cafecito a domicilio...');
+    }
+}
+
+
+window.addEventListener('load', function () {
     let stringIngresoDeUsuario = IngresoDeUsuario();
     let stringQueComprar = QueComprar();
+    let stringSeguirCompra = SeguirCompra();
 })
+
